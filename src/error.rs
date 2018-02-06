7518637d2eb86a1bd8cad213e50256f5a09bc613
Jan-Erik Rediger <janerik@fnordig.de>
2018-02-06 20:26:14 +0100

    Minimal client implementation for DNS over HTTPS
    Based on https://tools.ietf.org/html/draft-ietf-doh-dns-over-https-02

---
+++ src/error.rs
@@ -0,0 +1,22 @@
+use trust_dns_proto::error::ProtoError;
+use reqwest;
+
+#[derive(Fail, Debug)]
+pub enum Error {
+    #[fail(display = "Failed to parse DNS protocol")]
+    DnsProtoError,
+    #[fail(display = "{}", _0)]
+    Reqwest(#[cause] reqwest::Error),
+}
+
+impl From<ProtoError> for Error {
+    fn from(_error: ProtoError) -> Error {
+        Error::DnsProtoError
+    }
+}
+
+impl From<reqwest::Error> for Error {
+    fn from(error: reqwest::Error) -> Error {
+        Error::Reqwest(error)
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,71 @@
+extern crate trust_dns;
+extern crate trust_dns_proto;
+extern crate base64;
+extern crate reqwest;
+#[macro_use]
+extern crate failure;
+
+use std::str::FromStr;
+use std::net::IpAddr;
+
+use base64::encode;
+use reqwest::header::Host;
+
+use trust_dns::op::{Message, Query};
+use trust_dns::rr::{Name, RecordType};
+use trust_dns::rr::RData::*;
+
+mod error;
+use error::Error;
+
+const DNS_HOSTNAME : &str = "dns.google.com";
+const DNS_QUERY_URL : &str = "https://172.217.21.110/experimental";
+
+pub fn resolve_host(host: &str) -> Result<Vec<IpAddr>, Error> {
+    let client = reqwest::Client::builder()
+        .danger_disable_hostname_verification()
+        .build()?;
+
+    let mut ipv6 = resolve_host_family(&client, RecordType::AAAA, host)?;
+    let ipv4 = resolve_host_family(&client, RecordType::A, host)?;
+
+    ipv6.extend(ipv4);
+    Ok(ipv6)
+}
+
+fn resolve_host_family(client: &reqwest::Client, af: RecordType, name: &str) -> Result<Vec<IpAddr>, Error> {
+    let qname = Name::from_str(name)?;
+    let query = Query::query(qname, af);
+    let mut msg = Message::new();
+    msg.set_recursion_desired(true);
+    msg.add_query(query);
+
+    let qbuf = msg.to_vec()?;
+    let encoded = encode(&qbuf);
+
+    let mut resp = client.get(DNS_QUERY_URL)
+                           .query(&[
+                                  ("ct", ""),
+                                  ("body", &encoded)
+                           ])
+                           .header(Host::new(DNS_HOSTNAME, None))
+                           .send()?;
+
+    let mut body = Vec::new();
+    resp.copy_to(&mut body)?;
+    let msg = Message::from_vec(&body)?;
+
+    let results = msg.answers()
+        .iter()
+        .map(|answer| answer.rdata())
+        .flat_map(|data| {
+            match *data {
+                A(ipv4) => Some(IpAddr::V4(ipv4)),
+                AAAA(ipv6) => Some(IpAddr::V6(ipv6)),
+                _ => None
+            }
+        })
+        .collect();
+
+    Ok(results)
+}
